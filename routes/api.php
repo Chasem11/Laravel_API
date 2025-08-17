6f0eead59c5c4c168f0072276c3b9b17fcd902f0
Chasem11 <chasemoffat11@gmail.com>
2025-08-16 22:49:03 -0400

    added routes for auth. Chnaged vlaues in auth config file.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('getSession', 'App\Http\Controllers\AuthController@getSession');
+
+});
+
 Route::prefix('v1')->group(function () {
     Route::get('/users', 'App\Http\Controllers\GetRoutesController@getUsers');
     Route::get('/books', 'App\Http\Controllers\GetRoutesController@getBooks');
